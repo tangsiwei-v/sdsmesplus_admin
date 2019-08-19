c64824ce471ed31ff123f9cae035a85f42aa51e8
shangyanbing <g17317115947@163.com>
2019-08-19 16:29:54 +0800

    feature: 条码模板增删改

--- src/main/java/com/uspring/sdsmesplus/controller/PackBoxController.java
+++ src/main/java/com/uspring/sdsmesplus/controller/PackBoxController.java
@@ -19,7 +19,6 @@ import org.springframework.web.bind.annotation.*;
 import com.github.pagehelper.PageInfo;
 import com.uspring.sdsmesplus.entity.po.BoxInfoPO;
 import com.uspring.sdsmesplus.entity.po.ProdBoxLogPO;
-import com.uspring.sdsmesplus.entity.po.SysDeviceVerifyItemPO;
 import com.uspring.sdsmesplus.entity.vo.Result;
 import com.uspring.sdsmesplus.enums.StatusCode;
 import com.uspring.sdsmesplus.service.PackBoxService;
@@ -91,7 +90,7 @@ public class PackBoxController extends BaseController {
 	public Result searchBoxInfo(HttpServletResponse response, @PathVariable("prod_code") String prod_code,
 			@RequestParam(value = "customer_code", required = true) String customer_code,
 			@RequestParam(value = "fc_id", required = false) Integer fc_id) {
-		BoxInfoPO queryboxLog = packBoxService.queryboxLog(prod_code,customer_code,fc_id);
+		BoxInfoPO queryboxLog = packBoxService.queryboxLog(prod_code, customer_code, fc_id);
 		return new Result("sucess", queryboxLog, StatusCode.SUCCESS);
 	}
 
@@ -129,21 +128,19 @@ public class PackBoxController extends BaseController {
 	@ResponseBody
 	@RequestMapping(value = "/item", method = RequestMethod.POST)
 	@ApiOperation(value = "添加装箱数主数据", notes = "添加装箱数主数据", response = Result.class)
-	public Result insertBoxInfo(HttpServletResponse response,
-			@RequestBody BoxInfoPO boxInfo) {
+	public Result insertBoxInfo(HttpServletResponse response, @RequestBody BoxInfoPO boxInfo) {
 		packBoxService.insertBoxInfo(boxInfo);
 		return new Result("添加成功", "success", StatusCode.SUCCESS);
 	}
-	
+
 	@ResponseBody
 	@RequestMapping(value = "/item", method = RequestMethod.PUT)
 	@ApiOperation(value = "修改装箱数主数据", notes = "修改装箱数主数据", response = Result.class)
-	public Result updateBoxInfo(HttpServletResponse response,
-			@RequestBody BoxInfoPO boxInfo) {
+	public Result updateBoxInfo(HttpServletResponse response, @RequestBody BoxInfoPO boxInfo) {
 		packBoxService.updateBoxInfo(boxInfo);
 		return new Result("修改成功", "success", StatusCode.SUCCESS);
 	}
-	
+
 	@ResponseBody
 	@RequestMapping(value = "/item/{bifId}", method = RequestMethod.DELETE)
 	@ApiOperation(value = "删除装箱数主数据", notes = "删除装箱数主数据", response = Result.class)
@@ -151,7 +148,7 @@ public class PackBoxController extends BaseController {
 		packBoxService.deleteBoxInfo(bifId);
 		return new Result("删除成功", "success", StatusCode.SUCCESS);
 	}
-	
+
 	// 主数据查询
 	@ResponseBody
 	@RequestMapping(value = "/standard", method = RequestMethod.GET)
@@ -162,8 +159,8 @@ public class PackBoxController extends BaseController {
 			@RequestParam(value = "fcId", required = false) Integer fcId,
 			@RequestParam(value = "page_size", required = false) Integer page_size,
 			@RequestParam(value = "page_num", required = false) Integer page_num) {
-		PageInfo<BoxInfoPO> queryboxLog = packBoxService.standard(prod_code, customer_code, fcId,page_size,page_num);
+		PageInfo<BoxInfoPO> queryboxLog = packBoxService.standard(prod_code, customer_code, fcId, page_size, page_num);
 		return new Result("sucess", queryboxLog, StatusCode.SUCCESS);
 	}
-	
+
 }
